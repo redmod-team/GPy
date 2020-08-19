8d616ce538adcbe775c0831505d192ba772aa21e
manal44 <66592517+manal44@users.noreply.github.com>
2020-08-19 03:45:37 +0200

    Update stationary.py
    Cosine: description, dK_dX, dK_dX2, dK2_dXdX2
    Sinus: description, init, K_of_r, dK_dr, dK_dX, dK_dX2, dK2_dXdX2
    (still on progress: problem when r contains a 0)

--- GPy/kern/src/stationary.py
+++ GPy/kern/src/stationary.py
@@ -641,6 +641,16 @@ class ExpQuad(Stationary):
         return -r*self.K_of_r(r)
 
 class Cosine(Stationary):
+    """
+    Cosine kernel:
+
+    .. math::
+
+       k(r) = \sigma^2 \cos \\bigg( r \\bigg)
+       
+       r(x, x') = \\sqrt{ \\sum_{q=1}^Q (x_q - x'_q)^2 }
+
+    """
     def __init__(self, input_dim, variance=1., lengthscale=None, ARD=False, active_dims=None, name='Cosine'):
         super(Cosine, self).__init__(input_dim, variance, lengthscale, ARD, active_dims, name)
 
@@ -649,6 +659,80 @@ class Cosine(Stationary):
 
     def dK_dr(self, r):
         return -self.variance * np.sin(r)
+    
+    def dK_dX(self, X, X2, dimX):
+        r = self._scaled_dist(X, X2)
+        dK_dr = self.dK_dr(r)
+        dist = X[:,None,dimX]-X2[None,:,dimX]
+        lengthscale2inv = (np.ones((X.shape[1]))/(self.lengthscale**2))[dimX]
+        return 2. * lengthscale2inv * dist * r**(-1) * dK_dr
+    
+    def dK_dX2(self,X,X2,dimX2):7
+        return -1.*self.dK_dX(X,X2, dimX2)
+    
+    def dK2_dXdX2(self, X, X2, dimX, dimX2):
+        if X2 is None:
+            X2 = X
+        r = self._scaled_dist(X, X2)
+        dist = X[:,None,:]-X2[None,:,:]
+        lengthscale2inv = np.ones((X.shape[1]))/(self.lengthscale**2)
+        K = self.K_of_r(r)
+        dK_dr = self.dK_dr(r)
+        dist = X[:,None,:]-X2[None,:,:]
+        lengthscale2inv = np.ones((X.shape[1]))/(self.lengthscale**2)
+        l1 = lengthscale2inv[dimX]
+        l2 = lengthscale2inv[dimX2]
+        d1 = dist[:,:,dimX]
+        d2 = dist[:,:,dimX]
+        return (dimX==dimX2)*(-2.)*l1*d1*r**(-1)*dK_dr + l1*d1*r**(-1) * (2.*l1*d1*r**(-1)*dK_dr + 4*l2*d2*K)
+
+
+class Sinus(Stationary):
+    """
+    Sinus kernel:
+
+    .. math::
+
+       k(r) = \sigma^2 \sin \\bigg( r \\bigg)
+       
+       r(x, x') = \\sqrt{ \\sum_{q=1}^Q (x_q - x'_q)^2 }
+
+    """
+    
+    def __init__(self, input_dim, variance=1., lengthscale=None, ARD=False, active_dims=None, name='Sinus'):
+        super(Sinus, self).__init__(input_dim, variance, lengthscale, ARD, active_dims, name)
+    
+    def K_of_r(self, r):
+        return self.variance * np.sin(r)
+
+    def dK_dr(self, r):
+        return self.variance * np.cos(r)
+    
+    def dK_dX(self, X, X2, dimX):
+        r = self._scaled_dist(X, X2)
+        dK_dr = self.dK_dr(r)
+        dist = X[:,None,dimX]-X2[None,:,dimX]
+        lengthscale2inv = (np.ones((X.shape[1]))/(self.lengthscale**2))[dimX]
+        return 2.*lengthscale2inv*dist*r**(-1)*dK_dr
+    
+    def dK_dX2(self,X,X2,dimX2):
+        return -1.*self.dK_dX(X,X2, dimX2)
+    
+    def dK2_dXdX2(self, X, X2, dimX, dimX2):
+        if X2 is None:
+            X2 = X
+        r = self._scaled_dist(X, X2)
+        dist = X[:,None,:]-X2[None,:,:]
+        lengthscale2inv = np.ones((X.shape[1]))/(self.lengthscale**2)
+        K = self.K_of_r(r)
+        dK_dr = self.dK_dr(r)
+        dist = X[:,None,:]-X2[None,:,:]
+        lengthscale2inv = np.ones((X.shape[1]))/(self.lengthscale**2)
+        l1 = lengthscale2inv[dimX]
+        l2 = lengthscale2inv[dimX2]
+        d1 = dist[:,:,dimX]
+        d2 = dist[:,:,dimX]
+        return (dimX==dimX2)*(-2.)*l1*d1*r**(-1)*dK_dr + l1*d1*r**(-1) * (2.*l1*d1*r**(-1)*dK_dr + 4*l2*d2*K)
 
 
 class RatQuad(Stationary):
