76aedc0152d4e4016b2ab887ea94278c20f79eeb
manal44 <66592517+manal44@users.noreply.github.com>
2020-09-06 03:05:41 +0200

    Update expsin.py
    Documentation

--- GPy/kern/src/expsin.py
+++ GPy/kern/src/expsin.py
@@ -25,40 +25,43 @@ class ExpSin(Stationary):
     """
     def __init__(self, input_dim, variance=1., lengthscale=None, ARD=False, active_dims=None, name='ExpSin'):
         super(ExpSin, self).__init__(input_dim, variance, lengthscale, ARD, active_dims, name)
+    
+    # X and X2 have the same shape
+    # X.ndim = X2.ndim = 2
+    # X.shape[1] = X2.shape[1] = dimension of the kernel 
 
-    def K_of_r(self, dist):
-        n = dist.shape[2]
-        s = 0
+    def K_of_r(self, dist):             # uses dist (distance) instead of r (radial distance)
+        n = dist.shape[2]               # dist's shape: (len(X), len(X2), dimension of the kernel)
+        s = 0                           # initialization of the sum
         for k in range(n):
-            s+= np.sin(dist[:,:,k])**2
+            s+= np.sin(dist[:,:,k])**2  # the sum of the squared sinus of the distances
         return self.variance * np.exp(-s/(2*self.lengthscale**2))
     
-    def K(self, X, X2):
-        dist = X[:,None,:]-X2[None,:,:]
-        return self.K_of_r(dist)
+    def K(self, X, X2):                 # redefined because it uses dist instead of r
+        dist = X[:,None,:]-X2[None,:,:] # distance between X and X2
+        return self.K_of_r(dist)        # returns the Covariance matrix K(X,X2)
 
-    def dK_dr(self,dist,dimX):
-        K = self.K_of_r(dist)
-        d1 = dist[:,:,dimX]
+    def dK_dr(self,dist,dimX):          # returns the partial derivative of the kernel wrt dist[:,:,dimX]
+        K = self.K_of_r(dist)           # K(X,X2)
+        d1 = dist[:,:,dimX]             # shape of d1: (len(X),len(X2))
         return -K*np.sin(d1)*np.cos(d1)/self.lengthscale**2
     
-    def dK_dX(self, X, X2, dimX):
-        dist = X[:,None,:]-X2[None,:,:]
-        dK_dr = self.dK_dr(dist,dimX)
-        return dK_dr
+    def dK_dX(self, X, X2, dimX):       # returns the 1st derivative of the kernel wrt X[:,dimX]
+        dist = X[:,None,:]-X2[None,:,:] # distance between X and X2
+        return self.dK_dr(dist,dimX)
     
-    def dK_dX2(self,X,X2,dimX2):
+    def dK_dX2(self,X,X2,dimX2):        # returns the 1st derivative of the kernel wrt X2[:,dimX2]
         return -self.dK_dX(X,X2, dimX2)
     
-    def dK2_dXdX2(self, X, X2, dimX, dimX2):
-        dist = X[:,None,:]-X2[None,:,:]
-        K = self.K_of_r(dist)
-        dK_dX = self.dK_dX(X,X2,dimX)
-        lengthscale2inv = np.ones((X.shape[1]))/(self.lengthscale**2)
-        l1 = lengthscale2inv[dimX]
-        l2 = lengthscale2inv[dimX2]
-        d1 = dist[:,:,dimX]
-        d2 = dist[:,:,dimX2]
+    def dK2_dXdX2(self, X, X2, dimX, dimX2):    # returns the 2nd derivative of the kernel wrt X[:,dimX] and X2[:,dimX2]
+        dist = X[:,None,:]-X2[None,:,:] # distance between X and X2
+        K = self.K_of_r(dist)           # K(X,X2)
+        dK_dX = self.dK_dX(X,X2,dimX)   # 1st derivative of the kernel wrt X[:,dimX]
+        lengthscale2inv = np.ones((X.shape[1]))/(self.lengthscale**2)   # squared inverse of the lengthscale
+        l1 = lengthscale2inv[dimX]      # lengthscale of the dimX-th part of the kernel
+        l2 = lengthscale2inv[dimX2]     # lengthscale of the dimX2-th part of the kernel
+        d1 = dist[:,:,dimX]             # distance of the dimX-th part of the kernel
+        d2 = dist[:,:,dimX2]            # distance of the dimX2-th part of the kernel
         s1 = np.sin(d1)
         c1 = np.cos(d1)
         return (dimX!=dimX2)*dK_dX*l2*np.sin(d2)*np.cos(d2) + (dimX==dimX2)*l1*(dK_dX*s1*c1+K*c1**2-K*s1**2)
